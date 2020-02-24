eeac41afc652ddd54c0b865e6d2b29b416f89589
locotor <locotor@outlook.com>
2020-02-24 22:30:49 +0800

    add group side navi page

--- src/app/pages/group/group.component.ts
+++ src/app/pages/group/group.component.ts
@@ -6,6 +6,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupComponent implements OnInit {
 
+  relevantNavs = [
+    { name: '首页', icon: 'home' },
+    { name: '我执行的', icon: 'check_box' },
+    { name: '我参与的', icon: 'check_box' },
+    { name: '我创建的', icon: 'check_box' },
+  ];
+
+  projects = [
+    { name: '项目1' },
+    { name: '项目2' },
+    { name: '项目3' },
+    { name: '项目4' },
+    { name: '项目5' }
+  ];
+
+  collaborators = [
+    { name: '张三' },
+    { name: '李四' },
+    { name: '王麻子' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
